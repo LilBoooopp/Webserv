1ef5960a1e52f8cdce8aace998a912cf66285b3f
LilBoooopp <cbopp@student.42lausanne.ch>
2025-12-05 18:31:19 +0100

    update no segfault but bind error

--- src/http/Connection.cpp
+++ src/http/Connection.cpp
@@ -7,8 +7,8 @@
 Connection::Connection(void)
     : headers_done(false), responded(false), peer_closed(false),
       close_after(false), state(READING_HEADERS), want_body(0),
-      is_chunked(false), res(200), serverIdx(-1), file_fd(-1),
-      file_remaining(0), streaming_file(false) {}
+      is_chunked(false), res(200), serverIdx(0), file_fd(-1), file_remaining(0),
+      streaming_file(false) {}
 
 void Connection::printStatus(const std::string &label) {
   Logger::simple("%s - in: %s out: %s last: %s state: %d", label.c_str(),

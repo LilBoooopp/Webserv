2d9c76387f6eb488846330e277f5c5af37fe118f
Giulio Valente <valente95giulio@gmail.com>
2025-12-09 14:57:33 +0100

    Default Config in main

--- src/main.cpp
+++ src/main.cpp
@@ -9,46 +9,43 @@
 #include <signal.h>
 #include <sys/socket.h>
 
+std::string getDefaultConfig() {
+	std::cout << "No config file given, would you like to use default.conf?" << std::endl;
+	std::string res;
+	std::cin >> res;
+	if (!std::strncmp(res.c_str(), "yes", 1))
+		return "default.conf";
+	return "";
+}
+
 int main(int argc, char **argv) {
-  (void)argc;
-  if (argc <= 1)
-    return (0);
-  {
-    Connection c;
-    size_t endp = 0;
-    c.in = "GET / HTTP/1.1\r\nHost: x\r\n\r\n";
-    if (!HttpParser::parse(c, endp))
-      return (1);
-  }
-  signal(SIGPIPE, SIG_IGN);
+	std::string confPath = argc < 2 ? getDefaultConfig() : argv[1];
+	if (confPath.empty())
+		return (1);
 
-  // uint32_t ip_be = htonl(INADDR_LOOPBACK); // 127.0.0.1
-  // uint16_t port_be = htons(8080);
+	Config config;
+	Server s;
 
-  Config config;
-  Server s;
+	if (getExtension(confPath, '.') != "conf") {
+		std::cout << "Invalid configuration file:  " << confPath << " (must be .conf)\n";
+		return (1);
+	}
+	if (!config.parse(confPath)) {
+		std::cerr << "Config error at line " << config.getErrorLine() << ": "
+			  << config.getErrorMessage() << std::endl;
+		return (1);
+	}
 
-  if (getExtension(argv[1], '.') != "conf") {
-    std::cout << "Invalid configuration file:  " << argv[1]
-              << " (must be .conf)\n";
-    return 0;
-  }
-  if (!config.parse(argv[1])) {
-    std::cerr << "Config error at line " << config.getErrorLine() << ": "
-              << config.getErrorMessage() << std::endl;
-    return (1);
-  }
+	config.debug_print();
 
-  config.debug_print();
+	std::vector<ServerConf> servers = config.getServers();
 
-  std::vector<ServerConf> servers = config.getServers();
-
-  if (!s.start(servers)) {
-    std::perror("webserv: start failed (is another instance running?");
-    return (1);
-  }
-  s.setConf(servers);
-  std::cout << "started" << std::endl;
-  s.run();
-  return (0);
+	if (!s.start(servers)) {
+		std::perror("webserv: start failed (is another instance running?");
+		return (1);
+	}
+	s.setConf(servers);
+	std::cout << "started" << std::endl;
+	s.run();
+	return (0);
 }

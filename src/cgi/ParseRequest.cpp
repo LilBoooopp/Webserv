449dd0c75b20993c636169eff901f205c886e2f6
LilBoooopp <cbopp@student.42lausanne.ch>
2025-12-09 14:48:39 +0100

    All merged

--- src/cgi/ParseRequest.cpp
+++ src/cgi/ParseRequest.cpp
@@ -1,29 +1,30 @@
 #include "cgi.hpp"
+#include <cstring>
 
 std::string getInterpreter(const std::string &path, const ServerConf &conf) {
-	size_t qpos = path.find('?');
-	size_t searchEnd = (qpos == std::string::npos) ? path.size() : qpos;
-	size_t dotPos = path.rfind('.', searchEnd);
-	if (dotPos == std::string::npos)
-		return "";
-	std::string ext = path.substr(dotPos + 1, searchEnd - dotPos - 1);
-	if (ext != "py" && ext != "php")
-		return "";
-	const LocationConf *loc = findLocation(conf, path);
-	if (!loc)
-		return "";
-	if (ext == "py" && loc->has_py)
-		return loc->py_path;
-	if (ext == "php" && loc->has_php)
-		return loc->php_path;
-	return "";
+  size_t qpos = path.find('?');
+  size_t searchEnd = (qpos == std::string::npos) ? path.size() : qpos;
+  size_t dotPos = path.rfind('.', searchEnd);
+  if (dotPos == std::string::npos)
+    return "";
+  std::string ext = path.substr(dotPos + 1, searchEnd - dotPos - 1);
+  if (ext != "py" && ext != "php")
+    return "";
+  const LocationConf *loc = findLocation(conf, path);
+  if (!loc)
+    return "";
+  if (ext == "py" && loc->has_py)
+    return loc->py_path;
+  if (ext == "php" && loc->has_php)
+    return loc->php_path;
+  return "";
 }
 
 bool is_cgi(const std::string &req_target, const ServerConf &cfg) {
-	return (!std::strncmp(req_target.c_str(), "/cgi/", 4));
-	std::string interpreter = getInterpreter(req_target, cfg);
-	if (!interpreter.empty())
-		return true;
+  return (!std::strncmp(req_target.c_str(), "/cgi/", 4));
+  std::string interpreter = getInterpreter(req_target, cfg);
+  if (!interpreter.empty())
+    return true;
 }
 
 static std::string extractArguments(std::string &pathToCgi) {
@@ -38,21 +39,22 @@ static std::string extractArguments(std::string &pathToCgi) {
   return queryString;
 }
 
-void parseCgiRequest(const std::string &target, std::string &dir, std::string &file,
-		     std::string &queryString, const ServerConf &conf) {
+void parseCgiRequest(const std::string &target, std::string &dir,
+                     std::string &file, std::string &queryString,
+                     const ServerConf &conf) {
 
-	std::string joined = safe_join_under_root(conf.root, target);
-	Logger::cgi("%s rooted %s%s%s -> %s%s", SERV_CLR, GREY, target.c_str(), TS, GREY,
-		    joined.c_str());
-	Logger::cgi("Parsing cgi request: \'%s\'\n", joined.c_str());
-	queryString = extractArguments(joined);
+  std::string joined = safe_join_under_root(conf.root, target);
+  Logger::cgi("%s rooted %s%s%s -> %s%s", SERV_CLR, GREY, target.c_str(), TS,
+              GREY, joined.c_str());
+  Logger::cgi("Parsing cgi request: \'%s\'\n", joined.c_str());
+  queryString = extractArguments(joined);
 
-	size_t pos = joined.find_last_of('/');
-	if (pos == std::string::npos) {
-		dir = "";
-		file = joined;
-		return;
-	}
-	dir = joined.substr(0, pos + 1);
-	file = joined.substr(pos + 1);
+  size_t pos = joined.find_last_of('/');
+  if (pos == std::string::npos) {
+    dir = "";
+    file = joined;
+    return;
+  }
+  dir = joined.substr(0, pos + 1);
+  file = joined.substr(pos + 1);
 }

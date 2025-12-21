0f0803b81b71736c323c139dc01fb15d0e5c17af
LilBoooopp <cbopp@student.42lausanne.ch>
2025-12-21 16:10:56 +0100

    handleMessages change

--- src/cgi/CgiData.hpp
+++ src/cgi/CgiData.hpp
@@ -3,24 +3,29 @@
 #include "Cgi.hpp"
 
 struct CgiData {
-	std::vector<pid_t> asyncPids_;
-	std::string method;
-	std::string requestUri;
-	std::string queryString;
-	std::string contentLength;
-	std::string contentType;
-	std::string interp;
-	std::string file;
-	std::string path;
-	Connection *conn;
-	int fd;
-	int readFd;
-	pid_t pid;
-	size_t start;
-	size_t bytesRead;
-	std::string out;
-	bool noRead;
-	CgiData() : conn(NULL), readFd(-1), pid(-1), start(0), bytesRead(0), noRead(false) {}
-	bool tryInit(Connection &c, int fd);
-	void log(bool execSuccess);
+  pid_t pid;
+  int readFd;
+  int writeFd;
+  size_t start;
+  size_t bytesRead;
+
+  std::vector<pid_t> asyncPids_;
+  std::string method;
+  std::string requestUri;
+  std::string queryString;
+  std::string contentLength;
+  std::string contentType;
+  std::string interp;
+  std::string file;
+  std::string path;
+
+  Connection *conn;
+  int fd;
+  std::string out;
+  bool noRead;
+  CgiData()
+      : pid(-1), readFd(-1), writeFd(-1), bytesRead(0), conn(NULL),
+        noRead(false) {}
+  bool tryInit(Connection &c, int fd);
+  void log(bool execSuccess);
 };

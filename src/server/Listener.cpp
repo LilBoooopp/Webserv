9c2dd137c0ff26e89b2cd37d8149c7acf52e91fc
gvalente <valente95giulio@gmail.com>
2026-02-03 14:51:08 +0100

    done

--- src/server/Listener.cpp
+++ src/server/Listener.cpp
@@ -5,24 +5,24 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// UNVALID USE OF F_GETFL ?
-// static int set_nonblock(int fd) {
-// 	int f = fcntl(fd, F_GETFL, 0);
-// 	return ((f >= 0 && fcntl(fd, F_SETFL, f | O_NONBLOCK) == 0) ? 0 : -1);
-// }
-
-int set_nonblock(int fd) {
-	return ((fcntl(fd, F_SETFL, O_NONBLOCK) == 0) ? 0 : -1);
+static int set_nonblock(int fd) {
+  int f = fcntl(fd, F_GETFL, 0);
+  return ((f >= 0 && fcntl(fd, F_SETFL, f | O_NONBLOCK) == 0) ? 0 : -1);
 }
 
+// Listener::Listener() : fd_(-1) {
+// 	fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+// 	int on = 1;
+// 	::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+// 	#ifdef SO_REUSEPORT
+// 		::setsockopt(fd_, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
+// 	#endif
+// }
 
 Listener::Listener() : fd_(-1) {
-	fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
-	int on = 1;
-	::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-	#ifdef SO_REUSEPORT
-		::setsockopt(fd_, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
-	#endif
+  fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+  int on = 1;
+  ::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 }
 
 Listener::~Listener() {
@@ -31,32 +31,34 @@ Listener::~Listener() {
 }
 
 Listener::Listener(const Listener &other) {
-	this->fd_ = other.fd_;
-	other.fd_ = -1;
+  this->fd_ = other.fd_;
+  other.fd_ = -1;
 }
 
 Listener &Listener::operator=(const Listener &other) {
-	if (this != &other) {
-		if (fd_ >= 0)
-			::close(fd_);
+  if (this != &other) {
+    if (fd_ >= 0)
+      ::close(fd_);
 
-		this->fd_ = other.fd_;
-		other.fd_ = -1;
-	}
-	return (*this);
+    this->fd_ = other.fd_;
+    other.fd_ = -1;
+  }
+  return (*this);
 }
 
-bool Listener::operator==(const int other) const { return (this->fd() == other); }
+bool Listener::operator==(const int other) const {
+  return (this->fd() == other);
+}
 
 bool Listener::bindAndListen(uint32_t ip_be, uint16_t port_be, int backlog) {
-	sockaddr_in sa;
-	std::memset(&sa, 0, sizeof(sa));
-	sa.sin_family = AF_INET;
-	sa.sin_addr.s_addr = ip_be;
-	sa.sin_port = port_be;
-	if (::bind(fd_, (sockaddr *)&sa, sizeof(sa)) < 0)
-		return (false);
-	if (::listen(fd_, backlog) < 0)
-		return (false);
-	return (set_nonblock(fd_) == 0);
+  sockaddr_in sa;
+  std::memset(&sa, 0, sizeof(sa));
+  sa.sin_family = AF_INET;
+  sa.sin_addr.s_addr = ip_be;
+  sa.sin_port = port_be;
+  if (::bind(fd_, (sockaddr *)&sa, sizeof(sa)) < 0)
+    return (false);
+  if (::listen(fd_, backlog) < 0)
+    return (false);
+  return (set_nonblock(fd_) == 0);
 }
